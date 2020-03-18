6fe692a2b6ee6df1b45baa5b50ddd9169369cff0
covercash2 <covercash2@gmail.com>
2020-03-17 21:26:04 -0700

    add tests to the line module

--- src/parser/line.rs
+++ src/parser/line.rs
@@ -28,7 +28,7 @@ pub fn parse_list_item(line: &str) -> Option<ListItem> {
         .or(None)
 }
 
-pub fn parse_ordered_item<'t>(line: &'t str) -> Option<(&'t str, &'t str)> {
+fn parse_ordered_item<'t>(line: &'t str) -> Option<(&'t str, &'t str)> {
     line.find('.')
         .map(|dot_index| line.split_at(dot_index + 1))
         .filter(|(bullet, _)| {
@@ -45,7 +45,7 @@ pub fn parse_ordered_item<'t>(line: &'t str) -> Option<(&'t str, &'t str)> {
         })
 }
 
-pub fn parse_unordered_item<'t>(
+fn parse_unordered_item<'t>(
     line: &'t str,
     possible_bullets: &[&str],
 ) -> Option<(&'t str, &'t str)> {
@@ -72,7 +72,7 @@ pub fn parse_status<'t>(text: &'t str, possible_states: &[&str]) -> Option<(&'t 
 /// parse tags from the end of `text` and
 /// return the trimmed `text` and the tags
 /// or `None` if there are no tags
-pub fn parse_tags<'t>(text: &'t str) -> Option<(Vec<&'t str>, &'t str)> {
+fn parse_tags<'t>(text: &'t str) -> Option<(Vec<&'t str>, &'t str)> {
     text.trim()
         .rfind(char::is_whitespace)
         .map(|i| text.split_at(i))
@@ -121,8 +121,54 @@ pub fn parse_header_line<'t>(line: &'t str, possible_states: &[&str]) -> Option<
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    const TEST_STATES: [&str; 3] = ["TODO", "STARTED", "DONE"];
+
+    const GOOD_HEADERS: [&str; 9] = [
+        "* a simple header",
+        "* TODO a simple todo header",
+        "* TODO a simple todo header :with:tags:",
+        "** second level header",
+        "*** third level header",
+        "** STARTED second level started todo",
+        "* STARTED first level started todo",
+        "* DONE first level done todo",
+        "* STARTED first level started :with:tags",
+    ];
+
+    const GOOD_LIST_ITEMS: [&str; 4] = [
+        "- list item",
+        "+ plus list item",
+        "1. numbered list item",
+        "6. sixth list item",
+    ];
+
     #[test]
     fn test_test() {
-	assert!(true);
+        assert!(true);
+    }
+
+    #[test]
+    fn test_good_headers() {
+        // check that all lines are headers
+        assert!(GOOD_HEADERS.iter().all(|line: &&str| {
+            let line_num = 0;
+            match parse_line(line_num, line, &TEST_STATES) {
+                Line::Header(_line_num, _header) => true,
+                _ => false,
+            }
+        }))
+    }
+
+    #[test]
+    fn test_good_list_items() {
+        assert!(GOOD_LIST_ITEMS.iter().all(|line: &&str| {
+            let line_num = 0;
+            match parse_line(line_num, line, &TEST_STATES) {
+                Line::ListItem(_line_num, _list_item) => true,
+                _ => false,
+            }
+        }))
     }
 }
